f37fa8b66cddf418f61f8b9ffd3b4874ca311d1f
irvan <irvanyusufcahyadi4@gmail.com>
2025-08-10 19:48:33 +0700

    add seo for detail service page

--- app/(app)/(client)/service/[id]/page.tsx
+++ app/(app)/(client)/service/[id]/page.tsx
@@ -1,6 +1,23 @@
 import ServiceDetail from "@/components/service/service-detail";
+import {
+  generateServiceDetailJsonLd,
+  generateServiceDetailMetadata,
+} from "./metadata";
+import ServicesServices from "@/services/service.service";
+import Script from "next/script";
 
 type Params = Promise<{ id: string }>;
+
+export async function generateMetadata({ params }: { params: Params }) {
+  try {
+    const service = await ServicesServices.getServiceDetail((await params).id);
+    if (!service) return { title: "Service Not Found" };
+
+    return generateServiceDetailMetadata(service.data);
+  } catch (error) {
+    return { title: "Service Not Found" };
+  }
+}
 
 export default async function ServiceDetailPage({
   params,
@@ -8,8 +25,17 @@ export default async function ServiceDetailPage({
   params: Params;
 }) {
   const { id } = await params;
+  const service = await ServicesServices.getServiceDetail(id);
+  const serviceJsonLd = generateServiceDetailJsonLd(service.data);
   return (
     <div>
+      <Script
+        id="service-jsonld"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(serviceJsonLd),
+        }}
+      />
       <ServiceDetail serviceId={id} />
     </div>
   );
